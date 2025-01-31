1937f78bb6bcc35afa38ac6a991969e88b2743d1
Sutichai <kazychild@hotmail.co.th>
2025-01-31 18:42:32 +0700

    refact: refactor some flies

---
+++ app/page.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <div>
+      <h1>Homepage</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga obcaecati
+        molestias voluptas? Temporibus alias nostrum, quaerat repudiandae dolore
+        qui exercitationem.
+      </p>
+    </div>
+  );
+};
+
+export default Homepage;

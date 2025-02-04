6e072bfbac4b3aaefab225b978506d955b8a3501
Sutichai <kazychild@hotmail.co.th>
2025-02-05 01:21:09 +0700

    feat: add icon link for assets to any website you pick

--- components/IndexComponents/footer-section/FooterSection.jsx
+++ components/IndexComponents/footer-section/FooterSection.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const FooterSection = () => {
@@ -6,47 +7,85 @@ const FooterSection = () => {
     <div className="">
       <div className="flex pt-[51px] pb-[49px] px-[131px] justify-between items-center max-md:px-[40px] max-md:flex-col max-md:py-[40px] max-md:gap-[20px] h-[125px] bg-[#74C69D] max-sm:px-[20px]">
         <div className="flex gap-5">
-          <Image
-            src="/assets/icon/FacebookIcon.svg"
-            width={9.4}
-            height={18}
-            alt="facebook"
-          />
-          <Image
-            src="/assets/icon/InstagramIcon.svg"
-            width={18}
-            height={18}
-            alt="facebook"
-          />
-          <Image
-            src="/assets/icon/TwitterIcon.svg"
-            width={21.87}
-            height={18}
-            alt="twitter"
-          />
-          <Image
-            src="/assets/icon/PinterestIcon.svg"
-            width={13.94}
-            height={18}
-            alt="pinterest"
-          />
-          <Image
-            src="/assets/icon/WechatIcon.svg"
-            width={17.96}
-            height={18}
-            alt="wechat"
-          />
-          <Image
-            src="/assets/icon/YoutubeIcon.svg"
-            width={25.71}
-            height={18}
-            alt="youtube"
-          />
+          <Link
+            href="https://www.facebook.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/assets/icon/FacebookIcon.svg"
+              width={9.4}
+              height={18}
+              alt="facebook"
+              className="hover:opacity-70 transition-opacity duration-300"
+            />
+          </Link>
+          <Link
+            href="https://www.instagram.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/assets/icon/InstagramIcon.svg"
+              width={18}
+              height={18}
+              alt="facebook"
+              className="hover:opacity-70 transition-opacity duration-300"
+            />
+          </Link>
+          <Link href="https://x.com/" target="_blank" rel="noopener noreferrer">
+            <Image
+              src="/assets/icon/TwitterIcon.svg"
+              width={21.87}
+              height={18}
+              alt="twitter"
+              className="hover:opacity-70 transition-opacity duration-300"
+            />
+          </Link>
+          <Link
+            href="https://www.pinterest.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/assets/icon/PinterestIcon.svg"
+              width={13.94}
+              height={18}
+              alt="pinterest"
+              className="hover:opacity-70 transition-opacity duration-300"
+            />
+          </Link>
+          <Link
+            href="https://www.wechat.com/en/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/assets/icon/WechatIcon.svg"
+              width={17.96}
+              height={18}
+              alt="wechat"
+              className="hover:opacity-70 transition-opacity duration-300"
+            />
+          </Link>
+          <Link
+            href="https://www.youtube.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/assets/icon/YoutubeIcon.svg"
+              width={25.71}
+              height={18}
+              alt="youtube"
+              className="hover:opacity-70 transition-opacity duration-300"
+            />
+          </Link>
         </div>
         <div>
-            <p className="text-white text-[20px] leading-[25px] font-normal">
-                © Start, 2022. All Rights Reserved.
-            </p>
+          <p className="text-white text-[20px] leading-[25px] font-normal">
+            © Start, 2022. All Rights Reserved.
+          </p>
         </div>
       </div>
     </div>

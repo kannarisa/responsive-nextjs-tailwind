7e873e6129c2d9124bfcb750a762fd62fdee4c26
Sutichai <kazychild@hotmail.co.th>
2025-02-05 04:03:33 +0700

    fix: fixing the lines

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,17 @@
 import { Work_Sans } from "next/font/google";
 import "./globals.css";
 
-const workSans = Work_Sans({
-  subsets: ["latin"],
-  weight: ["400"],
-  variable: "--font-work-sans",
-});
-
 export const metadata = {
   title: "Responsive Web",
   description: "This is for practice with Next.js and tailwindcss",
   icons: "/favicon/Start.ico",
 };
+
+const workSans = Work_Sans({
+  subsets: ["latin"],
+  weight: ["400"],
+  variable: "--font-work-sans",
+});
 
 export default function RootLayout({ children }) {
   return (

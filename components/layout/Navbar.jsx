0d91e1b2ce746a28a5e2e83118852c7e626f8c88
Sutichai <kazychild@hotmail.co.th>
2025-02-17 23:17:25 +0700

    fix

--- components/layout/Navbar.jsx
+++ components/layout/Navbar.jsx
@@ -12,52 +12,51 @@ const Navbar = () => {
 
   return (
     <div className="bg-[#74C69D]">
-      <div className="py-[35px] ml-[135px] mr-[130px] items-center max-md:mx-[40px] max-sm:mx-[20px]">
+      <div className="py-[35px] ml-[135px] mr-[130px] flex justify-between items-center max-md:mx-[40px] max-sm:mx-[20px]">
         {/* LOGO */}
-        <nav className="flex justify-center items-center mx-auto">
-          <div className="flex mr-auto">
-            <Link href="/">
-              <Image
-                src="/assets/logo/logo-start.svg"
-                width={70}
-                height={48.31}
-                alt="logo"
-                className="max-sm:w-11"
-              />
-            </Link>
 
-            <h1
-              className={`${workSans.className}  font-medium text-white text-[40px] leading-[50px] pl-[18px] max-sm:text-[30px] max-sm:leading-[37.5px] max-sm:pl-3`}
+        <div className="flex items-center">
+          <Link href="/">
+            <Image
+              src="/assets/logo/logo-start.svg"
+              width={70}
+              height={48.31}
+              alt="logo"
+              className="max-sm:w-11"
+            />
+          </Link>
+
+          <h1
+            className={`${workSans.className}  font-medium text-white text-[40px] leading-[50px] pl-[18px] max-sm:text-[30px] max-sm:leading-[37.5px] max-sm:pl-3`}
+          >
+            <Link href="/">Start</Link>
+          </h1>
+        </div>
+
+        {/* Desktop Navbar (แสดงเมื่อจอใหญ่กว่า 768px) */}
+        <nav className="hidden md:flex gap-[30px]">
+          <ul className="flex gap-[30px] bg-[#74C69D]">
+            <li
+              className={`${workSans.className} font-normal text-white text-[20px] leading-[25px] duration-200 hover:scale-[1.1]`}
             >
-              <Link href="/">Start</Link>
-            </h1>
-          </div>
-
-          {/* Desktop Navbar (แสดงเมื่อจอใหญ่กว่า 768px) */}
-          <nav className="hidden md:flex gap-[30px]">
-            <ul className="flex gap-[30px] bg-[#74C69D]">
-              <li
-                className={`${workSans.className} font-normal text-white text-[20px] leading-[25px] duration-200 hover:scale-[1.1]`}
-              >
-                <Link href="/">Home</Link>
-              </li>
-              <li
-                className={`${workSans.className} font-normal text-white text-[20px] leading-[25px] duration-200 hover:scale-[1.1]`}
-              >
-                <Link href="/portfolio">Portfolio</Link>
-              </li>
-              <li
-                className={`${workSans.className} font-normal text-white text-[20px] leading-[25px] duration-200 hover:scale-[1.1]`}
-              >
-                <Link href="/services">Services</Link>
-              </li>
-              <li
-                className={`${workSans.className} font-normal text-white text-[20px] leading-[25px] duration-200 hover:scale-[1.1]`}
-              >
-                <Link href="/contact-us">Contact</Link>
-              </li>
-            </ul>
-          </nav>
+              <Link href="/">Home</Link>
+            </li>
+            <li
+              className={`${workSans.className} font-normal text-white text-[20px] leading-[25px] duration-200 hover:scale-[1.1]`}
+            >
+              <Link href="/portfolio">Portfolio</Link>
+            </li>
+            <li
+              className={`${workSans.className} font-normal text-white text-[20px] leading-[25px] duration-200 hover:scale-[1.1]`}
+            >
+              <Link href="/services">Services</Link>
+            </li>
+            <li
+              className={`${workSans.className} font-normal text-white text-[20px] leading-[25px] duration-200 hover:scale-[1.1]`}
+            >
+              <Link href="/contact-us">Contact</Link>
+            </li>
+          </ul>
         </nav>
         {/* Mobile Hamburger Menu (แสดงเมื่อจอน้อยกว่า 768px) */}
         <nav className="md:hidden">

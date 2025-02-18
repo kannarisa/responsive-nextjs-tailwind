894db04a8c203c39b64b437ed742c83bf4ea0fcf
Sutichai <kazychild@hotmail.co.th>
2025-02-18 18:17:43 +0700

    feat: add component Map

---
+++ app/contact-us/components/MapContact.jsx
@@ -0,0 +1,17 @@
+const MapContact = () => {
+  return (
+    <div>
+      <div className="">
+        <iframe
+          className="w-[575px] h-[384px] border-0 rounded-lg max-w-full"
+          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15502.150262417772!2d100.492773!3d13.746424!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e299057145cc81%3A0x7df49a08f511c22e!2z4Lin4Lix4LiU4Lie4Lij4Liw4LmA4LiK4LiV4Li44Lie4LiZ4Lin4Li04Lih4Lil4Lih4Lix4LiH4LiE4Lil4Liy4Lij4Liy4LihICjguKfguLHguJTguYLguJ7guJjguLTguYwp!5e0!3m2!1sth!2sth!4v1738697005600!5m2!1sth!2sth"
+          allowFullScreen
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
+    </div>
+  );
+};
+
+export default MapContact;
